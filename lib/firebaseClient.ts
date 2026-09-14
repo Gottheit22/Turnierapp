cfe0b53af56a4310fc5efe4f2e599b7cf151fc31
Gottheit22 <88936418+Gottheit22@users.noreply.github.com>
2026-09-14 16:25:31 +0200

    Update firebaseClient.ts

--- lib/firebaseClient.ts
+++ lib/firebaseClient.ts
@@ -1,5 +1,6 @@
 import { getApps, getApp, initializeApp } from 'firebase/app';
 import { collection, getFirestore, doc } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -18,6 +19,7 @@ if (!firebaseConfig.apiKey || !firebaseConfig.projectId) {
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
 
 export const TOURNAMENTS_COLLECTION = 'tournaments';
 export const tournamentsCollectionRef = collection(db, TOURNAMENTS_COLLECTION);
